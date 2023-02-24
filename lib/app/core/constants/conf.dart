07677291a7e24326df162e88dd334854fb820872
ALLOWAKOU Ferdinand <allowakouferdinand@gmail.com>
2023-02-24 11:30:00 +0100

    MAJ

---
+++ lib/app/core/constants/conf.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: non_constant_identifier_names
+
+class ConfString {
+  static String BASE_URL = "http://votifyapp.pythonanywhere.com";
+  static String ELECTION_URL = "$BASE_URL/api/elections/";
+  static String REQUEST_URL = "$BASE_URL/api/requests/";
+  static String OPTIONS_URL = "$BASE_URL/api/options/";
+  static String VOTERS_URL = "$BASE_URL/api/voters/";
+  static String VOTES_URL = "$BASE_URL/api/votes/";
+  static String AUTH_URL = "$BASE_URL/auth/";
+  // static String r = "";
+  // static String r = "";
+  // static String r = "";
+  // static String r = "";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+  // static String r = "r";
+}
